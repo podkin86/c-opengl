60004e8a9a82fe4bea7c40c55b89fa15be801e5b
podkin86 <ultrapodkin2@hmail.com>
2025-12-22 15:33:37 +0100

    add a multicolor

--- src/main.cpp
+++ src/main.cpp
@@ -106,13 +106,8 @@ int main()
     float vertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   // top 
-
-        // positions         // colors
-         0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
-         -0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // top left
-         0.5f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // bottom 
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 
     };
 
@@ -154,7 +149,7 @@ int main()
 
         // render the triangle
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
