ed2e70f8e31a520c9a0410eed1a75a2a3b87c13c
podkin86 <ultrapodkin2@hmail.com>
2025-12-31 17:53:58 +0100

    add a multicolor

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -107,7 +108,9 @@ int main()
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   // top 
+
+        
 
     };
 
